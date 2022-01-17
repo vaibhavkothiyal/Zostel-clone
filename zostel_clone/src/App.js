6e6a30bf36aa58f023b679e5bd941fd2328c8684
Vaibhav Kothiyal <vaibhavkkothiyal@gmail.com>
2022-01-17 20:46:06 +0530

    created_structure

---
+++ zostel_clone/src/App.js
@@ -0,0 +1,13 @@
+import { Navbar } from "./components/Navbar/Navbar";
+import { DirectRoutes } from "./components/routes/Routes";
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <DirectRoutes />
+    </div>
+  );
+}
+
+export default App;
---
+++ zostel_clone/src/components/routes/Routes.js
@@ -0,0 +1,9 @@
+import { Route, Routes } from "react-router-dom";
+
+export const DirectRoutes=()=>{
+    return (
+        <Routes>
+            {/* <Route path="/" element={<Home />}></Route> */}
+        </Routes>
+    )
+}

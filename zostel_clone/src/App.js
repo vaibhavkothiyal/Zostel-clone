f67d19516460fd8dc872f051468e3476c776a8f4
Vaibhav Kothiyal <vaibhavkkothiyal@gmail.com>
2022-01-18 12:37:40 +0530

    updated

--- zostel_clone/src/App.js
+++ zostel_clone/src/App.js
@@ -1,9 +1,10 @@
+import './App.css'
 import { DirectRoutes } from "./components/routes/Routes";
 import { Home } from './components/Home/Home'
 
 function App() {
   return (
-    <div>
+    <div >
       <Home />
       <DirectRoutes />
     </div>

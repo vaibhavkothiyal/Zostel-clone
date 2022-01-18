fbd0dd6d76408c8bc81214258d53d3de0fadc411
shubhamtammewar <91666990+shubhamtammewar@users.noreply.github.com>
2022-01-18 14:21:51 +0530

    Update App.js

--- zostel_clone/src/App.js
+++ zostel_clone/src/App.js
@@ -1,12 +1,14 @@
 import './App.css'
 import { DirectRoutes } from "./components/routes/Routes";
 import { Home } from './components/Home/Home'
+import UpperDescription from './components/UpperDescription/UpperDescription';
 
 function App() {
   return (
     <div >
       <Home />
       <DirectRoutes />
+      <UpperDescription />
     </div>
   );
 }

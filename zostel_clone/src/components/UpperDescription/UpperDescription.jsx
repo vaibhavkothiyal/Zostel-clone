7beccedd6bfff22ba9023fa78ee8149f686ea876
shubhamtammewar <91666990+shubhamtammewar@users.noreply.github.com>
2022-01-18 14:21:41 +0530

    Create UpperDescription.jsx

---
+++ zostel_clone/src/components/UpperDescription/UpperDescription.jsx
@@ -0,0 +1,16 @@
+import './UpperDescription.css';
+export const UpperDescription=()=>{
+    return (<div>
+        <div>
+            <div><img src="https://img.cdn.zostel.com/zostel/gallery/images/kR55bIDpRYC-2wYqnlT6jA/manali-20201112102605.jpg?w=1280" alt="" /></div>
+            <div>
+               <div><img src="https://img.cdn.zostel.com/zostel/gallery/images/x7JkdsaZR-Se_PPX0NBN2g/manali-exterior-full-20201105110532.jpg?w=1280" alt="" /></div>
+               <div><img src="https://img.cdn.zostel.com/zostel/gallery/images/2B_i6AW9RTaNmY6d-2Bcmw/manali-board-20201105110533.jpg?w=1280" alt="" /></div>
+               <div><img src="https://img.cdn.zostel.com/zostel/gallery/images/15R001HDS2Cc_wCv97eAPQ/manali-garden-cafe-20201105110535.jpg?w=1280" alt="" /></div>
+            </div>
+        </div>
+        <div></div>
+        <div></div>
+    </div>)
+}
+export default UpperDescription
